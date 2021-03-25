b377cc41a3d69edccfe7d5b56375a17955de13fb
Dukezo <dukezo@web.de>
2021-03-25 17:27:19 +0100

    Add HeaderHero component

---
+++ src/components/HeaderHero/HeaderHero.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import './HeaderHero.css';
+import bgImage from '../../assets/images/home-hero-bg.jpg';
+import {
+  Hero,
+  Header,
+  Button,
+  LanguagePicker,
+  Menu,
+  RedirectSignupForm,
+  Feature,
+} from '../../components';
+
+const FeatureHero = () => {
+  return (
+    <Hero image={bgImage}>
+      <Header>
+        <Menu items={[<LanguagePicker />, <Button>Sign In</Button>]} />
+      </Header>
+      <div className="HeaderHero__content">
+        <Feature title="Unlimited movies, TV shows, and more.">
+          Watch anywhere. Cancel anytime.
+        </Feature>
+        <RedirectSignupForm />
+      </div>
+    </Hero>
+  );
+};
+
+export default FeatureHero;
