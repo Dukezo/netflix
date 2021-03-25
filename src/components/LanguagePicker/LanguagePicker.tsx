d39a0e372d1c846a23bff11a721b475fb428fd12
Dukezo <dukezo@web.de>
2021-03-25 17:24:55 +0100

    Add LanguagePicker component

---
+++ src/components/LanguagePicker/LanguagePicker.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './LanguagePicker.css';
+
+const LanguagePicker = () => {
+  return (
+    <select className="LanguagePicker">
+      <option>English</option>
+    </select>
+  );
+};
+
+export default LanguagePicker;
