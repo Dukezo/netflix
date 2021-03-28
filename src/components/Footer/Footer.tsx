6f01bf89d184f518de77bf58e57a34fe223cc063
Dukezo <dukezo@web.de>
2021-03-28 23:43:35 +0200

    Pass language picker class name as prop to Footer component

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,21 +1,32 @@
 import React from 'react';
 import { GridMenu } from '..';
 import { MenuLink } from '../../types';
+import mergeClassNames from '../../utils/merge-class-names';
 import LanguagePicker from '../LanguagePicker/LanguagePicker';
 import './Footer.css';
 
 interface Props {
   menuLinks: MenuLink[][];
+  languagePickerClassName?: string;
 }
 
-const Footer: React.FC<Props> = ({ menuLinks, children }) => {
+const Footer: React.FC<Props> = ({
+  menuLinks,
+  languagePickerClassName,
+  children,
+}) => {
   return (
     <footer className="Footer">
       <h3 className="Footer__hotline">Questions? Call 0800-000-9677</h3>
       <div className="Footer__menu">
         <GridMenu links={menuLinks} />
       </div>
-      <LanguagePicker className="Footer__languagePicker" />
+      <LanguagePicker
+        className={mergeClassNames(
+          'Footer__languagePicker',
+          languagePickerClassName
+        )}
+      />
       {children}
     </footer>
   );
