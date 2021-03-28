df96c0e2d25410e96125b5ed9f0ae83812c96874
Dukezo <dukezo@web.de>
2021-03-28 22:29:57 +0200

    Remove unused imports

--- src/components/LanguagePicker/LanguagePicker.tsx
+++ src/components/LanguagePicker/LanguagePicker.tsx
@@ -1,8 +1,7 @@
-import React, { HTMLAttributes } from 'react';
+import React from 'react';
 import './LanguagePicker.css';
 import { IoMdArrowDropdown } from 'react-icons/io';
 import { IconContext } from 'react-icons';
-import { IoGlobeOutline } from 'react-icons/io5';
 import mergeClassNames from '../../utils/merge-class-names';
 
 const LanguagePicker: React.FC<React.LabelHTMLAttributes<HTMLLabelElement>> = ({
