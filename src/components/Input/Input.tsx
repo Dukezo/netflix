8473216aded3065ba8245377590671bb00dce70f
Dukezo <dukezo@web.de>
2021-03-29 01:44:25 +0200

    Generate unique IDs for each Input component

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,23 +1,35 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import mergeClassNames from '../../utils/merge-class-names';
 import './Input.css';
 
 interface Props {
   label: string;
+  containerClassName?: string;
 }
+
+let idCounter = 0;
 
 const Input = ({
   label,
+  containerClassName,
   ...props
 }: Props & React.InputHTMLAttributes<HTMLInputElement>) => {
+  const id = useRef(`input_${idCounter++}`);
+
   return (
-    <div className="Input">
+    <div className={mergeClassNames('Input', containerClassName)}>
       <input
         {...props}
         className={mergeClassNames('Input__inputField', props.className)}
         placeholder=" "
+        id={id.current}
+        onChange={() => {
+          console.log(idCounter);
+        }}
       />
-      <label className="Input__label">{label}</label>
+      <label className="Input__label" htmlFor={id.current}>
+        {label}
+      </label>
     </div>
   );
 };
