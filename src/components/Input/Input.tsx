624e956db42f4753466a46edd597cf07acfc1111
Dukezo <dukezo@web.de>
2021-03-29 15:52:24 +0200

    Forward input ref and add error prop

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -5,33 +5,45 @@ import './Input.css';
 interface Props {
   label: string;
   containerClassName?: string;
+  variant?: 'dark';
+  error?: string;
 }
 
+// Used to generate unique IDs for each input element.
 let idCounter = 0;
 
-const Input = ({
-  label,
-  containerClassName,
-  ...props
-}: Props & React.InputHTMLAttributes<HTMLInputElement>) => {
+const Input = React.forwardRef<
+  HTMLInputElement,
+  Props & React.InputHTMLAttributes<HTMLInputElement>
+>(({ label, containerClassName, variant, error, ...props }, ref) => {
   const id = useRef(`input_${idCounter++}`);
 
   return (
-    <div className={mergeClassNames('Input', containerClassName)}>
-      <input
-        {...props}
-        className={mergeClassNames('Input__inputField', props.className)}
-        placeholder=" "
-        id={id.current}
-        onChange={() => {
-          console.log(idCounter);
-        }}
-      />
-      <label className="Input__label" htmlFor={id.current}>
-        {label}
-      </label>
-    </div>
+    <>
+      <div
+        className={mergeClassNames('Input', containerClassName, {
+          'Input--dark': variant !== undefined,
+        })}
+      >
+        <div className="Input__inputFieldWrapper">
+          <input
+            {...props}
+            className={mergeClassNames('Input__inputField', props.className, {
+              'Input__inputField--hasError':
+                error !== undefined && error.length > 0,
+            })}
+            placeholder=" "
+            id={id.current}
+            ref={ref}
+          />
+          <label className="Input__label" htmlFor={id.current}>
+            {label}
+          </label>
+        </div>
+        {error && <div className="Input__error">{error}</div>}
+      </div>
+    </>
   );
-};
+});
 
 export default Input;
