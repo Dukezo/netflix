96b23240ad9870f4b71d1b6503380e0332d73151
Dukezo <dukezo@web.de>
2021-04-03 19:20:54 +0200

    Add new types and enums

---
+++ src/enums.ts
@@ -0,0 +1,27 @@
+export enum Genre {
+  Action = 'Action',
+  Adventure = 'Adventure',
+  Animation = 'Animation',
+  Comedy = 'Comedy',
+  Crime = 'Crime',
+  Documentary = 'Documentary',
+  Drama = 'Drama',
+  Family = 'Family',
+  Fantasy = 'Fantasy',
+  History = 'History',
+  Horror = 'Horror',
+  Music = 'Music',
+  Mystery = 'Mystery',
+  Romance = 'Romance',
+  ScienceFiction = 'Sci-Fi',
+  TVMovie = 'TV Movie',
+  Thriller = 'Thriller',
+  War = 'War',
+  Western = 'Western',
+  ActionAdventure = 'Action & Adventure',
+}
+
+export enum MediaType {
+  Movie,
+  TvShow,
+}
--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,5 @@
+import { Genre } from './enums';
+
 export interface AccordionData {
   title: string;
   description: string;
@@ -7,3 +9,47 @@ export interface MenuLink {
   title: string;
   href: string;
 }
+
+export interface TmdbEntity {
+  poster_path: string;
+  overview: string;
+  id: number;
+  popularity: number;
+  backdrop_path: string;
+  genre_ids: number[];
+  vote_average: number;
+  orignal_language: string;
+  vote_count: number;
+  media_type: TmdbMediaType;
+  genres: Genre[];
+}
+
+export interface TmdbMovie extends TmdbEntity {
+  release_date: string;
+  original_title: string;
+  title: string;
+  video: boolean;
+}
+
+export interface TmdbTvShow extends TmdbEntity {
+  first_air_date: string;
+  origin_country: string[];
+  name: string;
+  originalName: string;
+}
+
+export type TmdbMediaType = 'movie' | 'tv';
+export type TmdbTimeWindow = 'day' | 'week';
+
+export interface Medium {
+  id: number;
+  title: string;
+  image: string;
+  largeImage: string;
+  description: string;
+  rating: number;
+  genres: Genre[];
+}
+
+export interface Movie extends Medium {}
+export interface TVShow extends Medium {}
