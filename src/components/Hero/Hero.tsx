e94151eb644aa4ea74674703d954e22ca7738b28
Dukezo <dukezo@web.de>
2021-04-04 22:46:05 +0200

    Add spinner to Hero component

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,18 +1,26 @@
 import React from 'react';
+import { Spinner } from '..';
 import mergeClassNames from '../../utils/merge-class-names';
 import './Hero.css';
 
 interface Props {
   image?: string;
   className?: string;
+  spinnerVisible?: boolean;
 }
 
-const Hero: React.FC<Props> = ({ image, className, children }) => {
+const Hero: React.FC<Props> = ({
+  image,
+  className,
+  spinnerVisible,
+  children,
+}) => {
   return (
     <div
       className={mergeClassNames('Hero', className)}
       style={image ? { backgroundImage: `url(${image})` } : {}}
     >
+      {spinnerVisible && <Spinner />}
       <div className="Hero__content">{children}</div>
     </div>
   );
